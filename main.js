4b3445375a53b4aeeb88d8c370cb02eb34da1144
vijayavageesh17 <90508431+vijayavageesh17@users.noreply.github.com>
2022-01-22 16:56:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    width: 350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+ 
+function takesnapshot(){
+    
+        Webcam.snap(function (data_uri){
+         document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';   
+        });
+    }
+
+    console.log('ml5 version:',ml5.version);
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/78wR_muOi/model.json',modelLoaded);
+
+  function modelLoaded(){
+      console.log('model loaded');
+  }  
+
+  function identifyimage(){
+      img=document.getElementById("captured_image");
+      classifier.classify(img,gotResult);
+  }
+   
+  function gotResult(error,results){
+if (error){
+    console.error(error);
+} 
+ else {
+     console.log(results)
+      document.getElementById("result_object_name").innerHTML=results[0].label;
+      document.getElementById("accuracy_of_object").innerHTML=results[0].confidence.toFixed(3);
+   } }
+  
+
